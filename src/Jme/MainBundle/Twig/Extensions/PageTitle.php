da9c7078e441077b65db8e6eb4445bc6b53cc3dd
Janimatti Ellonen <janimatti.ellonen@soprano.fi>
2014-10-23 00:12:29 +0300

    Added page titles

---
+++ src/Jme/MainBundle/Twig/Extensions/PageTitle.php
@@ -0,0 +1,43 @@
+<?php
+namespace Jme\MainBundle\Twig\Extensions;
+
+use Symfony\Component\HttpFoundation\Session\Session;
+use Twig_Environment;
+use Twig_Extension;
+
+class PageTitle extends Twig_Extension
+{
+	/**
+	 * @var string
+	 */
+	private $siteName;
+
+	public function __construct($siteName)
+	{
+		$this->siteName = $siteName;
+	}
+	/**
+	 * @return array
+	 */
+	public function getFunctions()
+	{
+		return array(
+			'jme_page_title' => new \Twig_Function_Method(
+				$this, 'pageTitle', array('is_safe' => array('html') )
+			),
+		);
+	}
+
+	public function pageTitle($text)
+	{
+		return strlen(trim($text)) > 0 ? trim($text) . " | {$this->siteName}" : $this->siteName;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getName()
+	{
+		return 'jme_page_title';
+	}
+}
