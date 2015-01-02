e2c13a5d57a0ccb3fb42f43f248287dddde42134
Janimatti Ellonen <janimatti.ellonen@soprano.fi>
2015-01-03 00:45:24 +0200

    Initial new layout

--- src/Jme/MainBundle/Menu/Builder.php
+++ src/Jme/MainBundle/Menu/Builder.php
@@ -1,11 +1,12 @@
 <?php
 namespace Jme\MainBundle\Menu;
 
-use Knp\Menu\FactoryInterface,
-    Symfony\Component\HttpFoundation\Request,
-    Symfony\Component\DependencyInjection\ContainerAware,
-    Symfony\Component\Security\Core\SecurityContext,
-    Symfony\Bundle\FrameworkBundle\Translation\Translator;
+use Knp\Menu\FactoryInterface;
+use Symfony\Bundle\FrameworkBundle\Translation\Translator;
+use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
 
 class Builder extends ContainerAware
 {
@@ -24,16 +25,27 @@ class Builder extends ContainerAware
      */
     protected $translator;
 
+	/**
+	 * @var Router
+	 */
+	protected $router;
+
     /**
      * @param FactoryInterface $factory
      * @param SecurityContext $secutiryContext
      * @param Translator $translator
+	 * @param Router $router
      */
-    public function __construct(FactoryInterface $factory, SecurityContext $securityContext, Translator $translator)
+    public function __construct(
+		FactoryInterface $factory, 
+		SecurityContext $securityContext, 
+		Translator $translator, 
+		Router $router)
     {
         $this->factory          = $factory;
         $this->securityContext  = $securityContext;
         $this->translator       = $translator;
+		$this->router			= $router;
     }
 
     /**
@@ -45,18 +57,17 @@ class Builder extends ContainerAware
         $menu = $this->factory->createItem('root');
         $menu->setChildrenAttribute('class', 'nav navbar-nav');
 
-
-        $menu->addChild($this->translator->trans('menu.home'), array('route' => 'jme_article_latest') );
-        $menu->addChild($this->translator->trans('menu.about'), array('route' => 'JmeMainBundle_about') );
-        $menu->addChild($this->translator->trans('menu.projects'), array('route' => 'JmeMainBundle_projects') );
+        $menu->addChild($this->translator->trans('menu.home'), ['uri' => $this->router->generate('jme_article_latest') . '#a-home'] );
+        $menu->addChild($this->translator->trans('menu.about'), ['uri' => $this->router->generate('jme_article_latest') . '#a-about'] );
+        $menu->addChild($this->translator->trans('menu.projects'), ['uri' => $this->router->generate('jme_article_latest') . '#a-projects'] );
 
         if($this->securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED') )
         {
-            $menu->addChild($this->translator->trans('menu.article.new'), array('route' => 'jme_article_new') );
-            $menu->addChild($this->translator->trans('menu.media.media-bank'), array('route' => 'jme_media_list_files') );
-            $menu->addChild($this->translator->trans('menu.media.upload'), array('route' => 'jme_media_select_file') );
+            $menu->addChild($this->translator->trans('menu.article.new'), ['route' => 'jme_article_new'] );
+            $menu->addChild($this->translator->trans('menu.media.media-bank'), ['route' => 'jme_media_list_files'] );
+            $menu->addChild($this->translator->trans('menu.media.upload'), ['route' => 'jme_media_select_file'] );
 
-            $menu->addChild($this->translator->trans('menu.article.logout'), array('route' => 'fos_user_security_logout') );
+            $menu->addChild($this->translator->trans('menu.article.logout'), ['route' => 'fos_user_security_logout'] );
         }
 
         return $menu;
