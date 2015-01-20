b08728d4eb1e8aa342dc925555337c3bd14cbbd0
Janimatti Ellonen <janimatti.ellonen@soprano.fi>
2015-01-21 00:45:38 +0200

    Added an in-place html editor to the about box. Saving is not yet implemented.

---
+++ src/Jme/ArticleBundle/Resources/public/js/InPlaceEditor.js
@@ -0,0 +1,84 @@
+App.InPlaceEditor =  function(container) {
+    this.container = container;
+    this.content = null;
+    this.$textArea = null;
+    this.$innerContent = null;
+};
+
+App.InPlaceEditor.prototype = (function()
+{
+    return {
+        toTextArea: function() {
+            this.loadInnerContent();
+            this.content = this.$innerContent.html();
+            this.loadTextArea(this.content);
+            this.$innerContent.hide();
+            $(this.$textArea).show();
+        },
+
+        toHtml: function() {
+            this.$innerContent.html(this.$textArea.val());
+            this.$textArea.hide();
+            this.$innerContent.show();
+            this.disableEditControls();
+        },
+
+        saveChanges: function() {
+
+        },
+
+        cancelChanges: function() {
+            this.$textArea.hide();
+            this.$innerContent.html(this.content);
+            this.$textArea.val(this.content);
+            this.$innerContent.show();
+            this.disableEditControls();
+        },
+
+        loadInnerContent: function() {
+            if (null == this.$innerContent) {
+                this.$innerContent = $(this.container).find('.inner-content');
+            }
+        },
+
+        loadTextArea: function(content) {
+            if (null == this.$textArea) {
+                this.$textArea = $($('<textarea></textarea>').html(content));
+                this.$innerContent.after($(this.$textArea));
+            }
+        },
+
+        disableEditControls: function() {
+            $('.yes-btn', this.container).hide()
+            $('.no-btn', this.container).hide()
+            $('.in-place', this.container).show()
+        }
+    }
+})();
+
+$(document).ready(function() {
+    var inPlaceEditor = new App.InPlaceEditor('#a-about');
+
+    $('#a-about').on('click', '.in-place', function(e) {
+        e.preventDefault();
+
+        inPlaceEditor.toTextArea();
+        $('#a-about .yes-btn').show()
+        $('#a-about .no-btn').show()
+        $(this).hide();
+    });
+
+    $('#a-about').on('click', '.yes-btn', function(e) {
+        e.preventDefault();
+
+        inPlaceEditor.saveChanges();
+        inPlaceEditor.toHtml();
+    });
+
+    $('#a-about').on('click', '.no-btn', function(e) {
+        e.preventDefault();
+
+        inPlaceEditor.cancelChanges();
+        inPlaceEditor.toHtml();
+    });
+});
